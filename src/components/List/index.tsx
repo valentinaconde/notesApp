5b29ed9bee99b8b399f4904d76f74ab525071d42
valentinaconde <valencondde@gmail.com>
2024-07-29 16:09:44 -0300

    chore: Refactor list component and CSS file names and structure

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
+import './list.css'
 
 
 function ListComponent() {
   const [list, setList] = useState<string[]>([])
   const [value, setValue] = useState<string>('')
-  
+
   useEffect(() => {
     const list = localStorage.getItem('list')
-    if(list) {
+    if (list) {
       setList(JSON.parse(list))
     }
   }, [])
-  
+
   const handleSubmit = (event: any) => {
     event?.preventDefault()
-    if(!value) return
+    if (!value) return
     setList([...list, value])
     localStorage.setItem('list', JSON.stringify([...list, value]))
     setValue('')
@@ -27,25 +28,31 @@ function ListComponent() {
   }
 
   const handleDelete = (index: any) => {
-      const newList = list.filter((item, i) => i !== index)
-      setList(newList)
-      localStorage.setItem('list', JSON.stringify(newList))
+    const newList = list.filter((item, i) => i !== index)
+    setList(newList)
+    localStorage.setItem('list', JSON.stringify(newList))
   }
 
   const handleTacharEnIngles = (index: any) => {
-      document.getElementById(index)?.classList.add('tachado')
+    document.getElementById(index)?.classList.add('tachado')
   }
 
   return (
     <>
-      <form className='d-flex mb-3' onSubmit={handleSubmit}>
+      <form className='d-flex mb-3 list mt-5 ms-3' onSubmit={handleSubmit}>
         <input className='form-control' type="text" value={value} onChange={handleChange}></input>
-        <button  className='btn btn-success ms-2'>SUBMIT</button>
+        <button className='btn btn-success ms-2'>SUBMIT</button>
       </form>
 
       {
         list.map((item, index) => (
-          <div className='text-dark bg-light' key={index} id={`${index}`} >{item}<DeleteIcon className="icon ms-3 me-2" onClick={() => handleDelete(index)}/> <CloseIcon className='icon' onClick={() => handleTacharEnIngles(index)}/> </div>
+          <div className='text-dark bg-light list ms-3 mt-2 ps-2 d-flex justify-content-between' key={index} id={`${index}`} >
+            {item}
+            <div>
+              <DeleteIcon className="icon ms-3 me-2" onClick={() => handleDelete(index)} />
+              <CloseIcon className='icon' onClick={() => handleTacharEnIngles(index)} />
+            </div>
+          </div>
         ))
       }
     </>
