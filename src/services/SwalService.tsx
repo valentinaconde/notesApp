cb012d6e48d774fa7eb32e4c42ac2d4105929498
valentinaconde <valencondde@gmail.com>
2024-07-31 11:40:29 -0300

    refactor: Update SwalService to use specific alert types for confirmation and error messages

--- src/services/SwalService.tsx
+++ src/services/SwalService.tsx
@@ -1,21 +1,29 @@
 import Swal from 'sweetalert2'
 
-export function ConfirmAlert() {
+export function WarningAlert(title: string, text: string) {
     return Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
+        title: title,
+        text: text,
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonText: "Yes, do it!"
     })
 }
 
-export function DeletedAlert() {
+export function ConfirmAlert(title: string, text: string) {
     return Swal.fire({
-        title: "Deleted!",
-        text: "Your file has been deleted.",
+        title: title,
+        text: text,
         icon: "success"
+    });
+}
+
+export function ErrorAlert(title: string, text: string) {
+    return Swal.fire({
+        title: title,
+        text: text,
+        icon: "error"
     });
 }
